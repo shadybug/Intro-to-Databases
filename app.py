bc38e9d97303bc804b53935d0bc13600dd6d5a6f
Cicada Carpenter <cicada.carpenter@gmail.com>
2022-10-16 13:09:09 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+import mysql.connector
+import config
+
+app = flask.Flask(__name__)
+
+myexample_db = mysql.connector.connect(
+    host = "localhost",
+    user = config.username,
+    password = config.password
+)
+
+mycursor = myexample_db.cursor()
+mycursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase;")
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '3306'))
+    except ValueError:
+        PORT = 3306
+    app.run(HOST, PORT)
