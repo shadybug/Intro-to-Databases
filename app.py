1d6f99c7fde86ee780f93e94d0314ab5d1aa0f38
Cicada Carpenter <cicada.carpenter@gmail.com>
2023-02-26 13:04:52 -0700

    HTML Update

--- app.py
+++ app.py
@@ -10,15 +10,52 @@ myexample_db = mysql.connector.connect(
     password = config.password
 )
 
+# Database setup
+
 mycursor = myexample_db.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase;")
 mycursor.execute("USE mydatabase;")
 mycursor.execute("CREATE TABLE IF NOT EXISTS friends (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), major VARCHAR(255), age INT);")
+
+sql = "INSERT INTO friends (name, major, age) VALUES (%s, %s, %s)"
+value = [ ("Harry", "Astrophysics", 21), ("Laura", "Computer Science", 19), ("Peter", "Art", 17), ("Amy", "Chemistry", 26) ]
+
+mycursor.executemany(sql, value)
+
 myexample_db.commit()
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    # Select data from the database
+
+    sql = "SELECT name,major,age FROM friends"
+    mycursor.execute(sql)
+
+    myresult = mycursor.fetchall()
+
+    return flask.render_template("friends.html", list=myresult, title="All Friends")
+
+@app.route('/olderfriends')
+def olderfriends():
+    # Select data from the database
+
+    sql = "SELECT name,major,age FROM friends WHERE age > 20"
+    mycursor.execute(sql)
+
+    myresult = mycursor.fetchall()
+
+    return flask.render_template("friends.html", list=myresult, title="Friends Over 20")
+
+@app.route('/youngerfriends')
+def youngerfriends():
+    # Select data from the database
+
+    sql = "SELECT name,major,age FROM friends WHERE age < 20"
+    mycursor.execute(sql)
+
+    myresult = mycursor.fetchall()
+
+    return flask.render_template("friends.html", list=myresult, title="Friends Under 20")
 
 if __name__ == '__main__':
     import os
